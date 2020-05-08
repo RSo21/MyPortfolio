9a0db376a221e41eb29791bbf1e9c308023bc2f4
R <roksana_sobczak@wp.pl>
2020-05-08 19:43:30 +0200

    styles home main

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -1,16 +1,61 @@
 import React, { Component } from "react";
+import Navigation from "./Navigation";
+import TypingEffect from "./TypingEffect";
+
+//import myPhoto from '../assets/DSC_0562.jpg';
 
 
 
 class HomeMain extends Component {
 
+    state = {
+        width: window.innerWidth,
+    };
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
+
     render(){
-        return(
-            <>
-            </>
-        )
+        const { width } = this.state;
+        const isMobile = width <= 978;
+
+        if(isMobile){
+            return(
+                <>
+                </>
+            )
+        }else{
+            return(
+                <div className="home-main-container" id="home">
+                    <Navigation />
+                    <div className="home-main-content">
+                        <div className="home-main-image">
+                            {/* <img className="my-photo" src={myPhoto} alt="myPhoto"/> */}
+
+                        </div>
+                        <div className="home-main-text">
+                            <TypingEffect />
+                            <p>I'm ROKSANA</p>
+                            <p>Junior Front-end developer</p>
+                            <p>looking for a first job</p>
+                        </div>
+
+                    </div>
+                </div>     
+            )
+        }
     }
 }
+
          
     
 
