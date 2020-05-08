6ceb66ccd41488aacd381004577d5f65fafc3200
R <roksana_sobczak@wp.pl>
2020-05-08 19:42:37 +0200

    add nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 
 
-class HomeFooter extends Component {
+class Navigation extends Component {
     
     render(){
         return(
-             <div className= "nav">
-                <a href="/">HOME</a>
-                <a href = "#about">ABOUT ME</a>
-                <a href = "#skills">SKILLS</a>
-                <a href = "#portfolio">MY PORTFOLIO</a>
-                <a href = "#contact">CONTACT</a>
+            <div className="nav-container">
+                <div className= "nav">
+                    <a href="/">HOME</a>
+                    <a href = "#about">ABOUT ME</a>
+                    <a href = "#skills">SKILLS</a>
+                    <a href = "#portfolio">MY PORTFOLIO</a>
+                    <a href = "#contact">CONTACT</a>
+                </div>
             </div>
+            
              )
          }
     }
 
-export default HomeFooter;
+export default Navigation;
