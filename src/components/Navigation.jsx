a9707d409a5106289fe5aeb6c6eba6778fcab45d
R <roksana_sobczak@wp.pl>
2020-05-10 12:07:40 +0200

    add fonts and backgroundColor scroll nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,77 @@
 import React, { Component } from "react";
+import { slide as Menu } from "react-burger-menu";
+import MenuIcon from "../assets/menu.png";
 
 
 class Navigation extends Component {
+    state = {
+        width: window.innerWidth,
+        navBackground: "",
+        fontColor: ''
+    };
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+        document.addEventListener("scroll", () => {
+            const backgroundcolor = window.scrollY < 100 ? "transparent" : "rgba(255, 255, 255, 0.95)";
+            const navFont = window.scrollY < 100 ? " rgb(255, 255, 255)" : "rgb(0, 0, 0)";
+            this.setState({ 
+                navBackground: backgroundcolor,
+                fontColor: navFont,
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
     
     render(){
-        return(
-            <div className="nav-container">
-                <div className= "nav">
-                    <a href="/">HOME</a>
-                    <a href = "#about">ABOUT ME</a>
-                    <a href = "#skills">SKILLS</a>
-                    <a href = "#portfolio">MY PORTFOLIO</a>
-                    <a href = "#contact">CONTACT</a>
+        const { width, navBackground, fontColor } = this.state;
+        const isMobile = width <= 978;
+
+        if(isMobile){
+            return(
+                <div className="mobile-nav">
+                    <div><img className = "icon-menu-nav" src = {MenuIcon} alt = "menuIcon"/>
+                     </div>
+                     <Menu className = "menu-mobile" pageWrapId={"page-wrap"} isOpen={true} width={ 150 }>
+                         <p activeclass="active" spy="true" offset={50} duration={500}>
+                            <a href="/">HOME</a>
+                         </p>
+                         <p activeclass="active" spy="true" offset={50} duration={500}>
+                            <a href = "#about">ABOUT ME</a>
+                         </p>
+                         <p activeclass="active" spy="true" offset={50} duration={500}>
+                            <a href = "#skills">SKILLS</a>
+                         </p>
+                         <p activeclass="active" spy="true" offset={50} duration={500}>
+                            <a href = "#portfolio">MY PORTFOLIO</a>
+                         </p>
+                         <p activeclass="active" spy="true" offset={50} duration={500}>
+                            <a href = "#contact">CONTACT</a>
+                         </p>
+                     </Menu>
                 </div>
-            </div>
-            
+            )
+        }else{
+            return(
+                <div className="nav-container" style={{backgroundColor: navBackground}}>
+                    <div className= "nav">
+                        <a href="/" style={{color: fontColor}}>HOME</a>
+                        <a href = "#about" style={{color: fontColor}}>ABOUT ME</a>
+                        <a href = "#skills" style={{color: fontColor}}>SKILLS</a>
+                        <a href = "#portfolio" style={{color: fontColor}}>MY PORTFOLIO</a>
+                        <a href = "#contact" style={{color: fontColor}}>CONTACT</a>
+                    </div>
+                </div>
              )
          }
     }
+}
 
 export default Navigation;
