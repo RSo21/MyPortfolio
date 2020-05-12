8f9a14ba187e6cda7070451b52eb643b288c1dfe
R <roksana_sobczak@wp.pl>
2020-05-12 19:28:06 +0200

    add home about

--- src/components/HomeAbout.jsx
+++ src/components/HomeAbout.jsx
@@ -5,8 +5,28 @@ import React, { Component } from "react";
 class HomeAbout  extends Component{
     render(){
         return(
-            <>
-            </>
+            <div id="about">
+                <p className="about-title">ABOUT ME</p>
+                <div className="home-about-container">
+                    <div className="home-about-past">
+                        <p>THE PAST</p>
+                        <p></p>
+
+                    </div>
+                    <div className="home-about-present">
+                        <p>THE PRESENT</p>
+                        <p></p>
+
+                    </div>
+                    <div className="home-about-future">
+                        <p>THE FUTURE</p>
+                        <p></p>
+
+                    </div>
+
+                </div>
+
+            </div>
         )
     }
 }
