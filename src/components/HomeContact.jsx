aa820c4cbceb70bd9018d825fe7bb04e88a4d93b
R <roksana_sobczak@wp.pl>
2020-05-15 22:39:51 +0200

    add home contact styles

--- src/components/HomeContact.jsx
+++ src/components/HomeContact.jsx
@@ -7,9 +7,12 @@ class Home  extends Component{
         return(
             <div id="contact">
                 <p className="contact-title">CONTACT</p>
-                <p className="contact-element">Send me an email on: roksanasobczak11@gmail.com</p>
-                <p className="contact-element">Text me an: <a target="_blank" rel="noopener noreferrer" href ="https://www.linkedin.com/in/roksana-sobczak/"><i class="fab fa-linkedin"></i></a></p>
-                <p className="contact-element">.. or find me: <a target="_blank" rel="noopener noreferrer" href ="https://github.com/RSo21"><i class="fab fa-github"></i></a></p>
+                <div className="contact-container">
+                    <p className="contact-element">If you have any questions please feel free to contact me.</p>
+                    <p className="contact-element">Send me an email on: roksanasobczak11@gmail.com</p>
+                    <p className="contact-element">Text me a LinkedIn: <a target="_blank" rel="noopener noreferrer" href ="https://www.linkedin.com/in/roksana-sobczak/"><i class="fab fa-linkedin"></i></a></p>
+                    <p className="contact-element">.. or find me on GitHub: <a target="_blank" rel="noopener noreferrer" href ="https://github.com/RSo21"><i class="fab fa-github"></i></a></p>
+                </div>  
             </div>
         )
     }
