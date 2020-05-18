15ecb687635b65f977ad27f272dbac1bfbccff1b
R <roksana_sobczak@wp.pl>
2020-05-18 14:31:11 +0200

    Updates

---
+++ precache-manifest.ffd80cb188a75332b268a41785ddabd1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2315c11d84b13a28958528005a798971",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "f483d8578e906ac705e7",
+    "url": "/MyPortfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7ecfab230143d0dff95e",
+    "url": "/MyPortfolio/static/css/main.415aa26b.chunk.css"
+  },
+  {
+    "revision": "f483d8578e906ac705e7",
+    "url": "/MyPortfolio/static/js/2.63c85a70.chunk.js"
+  },
+  {
+    "revision": "2c21478778d46668a7c75a32a167f767",
+    "url": "/MyPortfolio/static/js/2.63c85a70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ecfab230143d0dff95e",
+    "url": "/MyPortfolio/static/js/main.91c0171e.chunk.js"
+  },
+  {
+    "revision": "5f8e92b29b644e1a2fde",
+    "url": "/MyPortfolio/static/js/runtime-main.09a4418b.js"
+  },
+  {
+    "revision": "3018d3dd65d3eb95cedd746946198457",
+    "url": "/MyPortfolio/static/media/carbonFootprint.3018d3dd.png"
+  },
+  {
+    "revision": "51ac7f34da98c8eb269ddcb92adb9637",
+    "url": "/MyPortfolio/static/media/catGame.51ac7f34.png"
+  },
+  {
+    "revision": "b443454156fdfc887d804e507cdc90c0",
+    "url": "/MyPortfolio/static/media/giveAwayApp.b4434541.png"
+  },
+  {
+    "revision": "3aa797f496bf276d969277660b81a72e",
+    "url": "/MyPortfolio/static/media/jarsSystem.3aa797f4.png"
+  },
+  {
+    "revision": "ed34d4f970c98d9e8b22df5c8c147774",
+    "url": "/MyPortfolio/static/media/kara-eads-FbXp5aOX-fs-unsplash.ed34d4f9.jpg"
+  },
+  {
+    "revision": "ac375a4c98111967393d59891c95457d",
+    "url": "/MyPortfolio/static/media/luca-bravo-anUkeEyWr28-unsplash.ac375a4c.jpg"
+  },
+  {
+    "revision": "dd3e12d3c0710e7342ff0c468f309334",
+    "url": "/MyPortfolio/static/media/myPhoto.dd3e12d3.jpg"
+  },
+  {
+    "revision": "f0901c46ab1c7ea8df8257384902debc",
+    "url": "/MyPortfolio/static/media/quino-al-4SNUcHPiC8c-unsplash.f0901c46.jpg"
+  },
+  {
+    "revision": "167c642cdbe22c0ebece06d1fd524b02",
+    "url": "/MyPortfolio/static/media/vafi.167c642c.png"
+  },
+  {
+    "revision": "7e3d4dcaa50fb042be1bf150ee300f75",
+    "url": "/MyPortfolio/static/media/weatherApp.7e3d4dca.png"
+  }
+]);
