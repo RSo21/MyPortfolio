2ecdbf44f8d459aaa393078ad61a393810861e63
R <roksana_sobczak@wp.pl>
2020-05-20 20:17:03 +0200

    footer change

--- src/components/HomeFooter.jsx
+++ src/components/HomeFooter.jsx
@@ -6,8 +6,7 @@ class HomeFooter extends Component{
     render(){
         return(
            <div className="home-footer-container">
-               <p>&copy; 2020 ROKSANA SOBCZAK | All Rights Reserved </p>
-               
+               <p>&copy; 2020 ROKSANA SOBCZAK | All Rights Reserved </p>  
            </div>
             )
         }
